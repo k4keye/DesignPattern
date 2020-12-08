0afe7a8047dc9c8eab9a4b26a4d38e03f53b5f10
k4keye <vps32@naver.com>
2020-12-08 22:18:21 +0900

    SingletonPattern

---
+++ SingletonPattern/SingletonPattern/Api.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Api
+    {
+        static private Api instance;
+
+        private Api()
+        {
+
+        }
+        public static Api GetInstance()
+        {
+            if(instance ==null)
+            {
+                instance = new Api();
+            }
+            return instance;
+        }
+        void Get()
+        {
+
+        }
+        void Post()
+        {
+
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //새로운 인스턴스를 생성하지않고
+            //인스턴스를 반환받는다.
+            Api api = Api.GetInstance();
+        }
+    }
+}
